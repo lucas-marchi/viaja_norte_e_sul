3d394fc5adfb925352b24c507a7b9500e8648116
¨lucas-marchi¨ <lucasmarchi20@gmail.com>
2024-08-18 14:26:20 -0300

    welcome page

---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+
+  List images = [
+    "welcome-one.jpg",
+    "welcome-two.jpg",
+    "welcome-three.jpg",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: images.length,
+        itemBuilder: (_, index){
+          return Container(
+            width: double.maxFinite,
+            height: double.maxFinite,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  "img/"+images[index]
+                  ),
+                fit: BoxFit.cover
+              )
+            ),
+          );
+      }),
+    );
+  }
+}
