6eabab7534d5a5e079999c0ff5fb3b271e9bee1c
lucas-marchi <lucasmarchi20@gmail.com>
2024-09-04 22:40:43 -0300

    Cubit implementado

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:viaja_norte_e_sul/cubit/app_cubit_logics.dart';
+import 'package:viaja_norte_e_sul/cubit/app_cubits.dart';
 import 'package:viaja_norte_e_sul/pages/detail_page.dart';
 import 'package:viaja_norte_e_sul/pages/navpages/main_page.dart';
 import 'package:viaja_norte_e_sul/pages/welcome_page.dart';
+import 'package:viaja_norte_e_sul/services/data_services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,13 +17,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-
-        primarySwatch: Colors.green,
-      ),
-      home: DetailPage()
-    );
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: BlocProvider<AppCubits>(
+          create: (context) => AppCubits(
+            data: DataServices(),
+          ),
+          child: AppCubitLogics(),
+        ));
   }
 }
